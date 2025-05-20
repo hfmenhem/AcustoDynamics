1bb0075177f5a593ef55f18bfdc65d74064642e6
hfmenhem <75636969+hfmenhem@users.noreply.github.com>
2025-05-20 16:11:45 -0300

    Achado efeito de batimento
    achei efeito de batimento quando fora de fase

--- Batimento/Batimento.py
+++ Batimento/Batimento.py
@@ -11,13 +11,13 @@ import matplotlib as mpl
 #Bolinhas de ABS 1,5mm (provavelmente de diâmetro)
 #propriedades: https://www.researchgate.net/figure/Density-and-sound-speed-values-Acrylonitrile-butadiene-styrene-ABS-polylactic-acid_tbl1_329699503
 
-#Npar = 2
-Npar = 1
+Npar = 2
+# Npar = 1
 
 dicMeio = Simulador.ar(Npar)
-#a = np.array([[1], [1]]) #np.array(Npar*[[1.5/2]]) # [mm]
-a = np.array([[1]]) #np.array(Npar*[[1.5/2]]) # [mm]
-rhoPol = (9000*(10**-6)) #[g/mm^3]
+a = np.array([[1], [1]]) #np.array(Npar*[[1.5/2]]) # [mm]
+# a = np.array([[1]]) #np.array(Npar*[[1.5/2]]) # [mm]
+rhoPol = (900*(10**-6)) #[g/mm^3]
 cPol = 2740*(10**3) #[mm/s] 
 m = (a**3*(4*np.pi/3))*rhoPol # [g], densidade do ar vezes seu volume
 
@@ -35,17 +35,17 @@ lamb = dicMeio["c"]/f
 print(f'lambda = {lamb:.2f} mm ')
 
 
-# r0 = np.array([[[0,0,0]], [[0,0,lamb/2]]])
-# v0 = np.array([[[0.0,0,0]], [[0.0,0,0]]])
+r0 = np.array([[[0,0,0-0.3]], [[0,0,(lamb) +0.3]]])
+v0 = np.array([[[0.0,0,0]], [[0.0,0,0]]])
 
-r0 = np.array([[[0,0,-1]]])
-v0 = np.array([[[0.0,0,0]]])
+# r0 = np.array([[[0,0,-1]]])
+# v0 = np.array([[[0.0,0,0]]])
 
 
 
-sim = Simulador(f1, f2, f, dicMeio['c'], a, m, dicMeio['rho'], v0max, h, dicMeio['dinvis']*100,e=0)
-dt = 0.00005
-tempo = 1
+sim = Simulador(f1, f2, f, dicMeio['c'], a, m, dicMeio['rho'], v0max, h, dicMeio['dinvis'],e=0)
+dt = 0.00001
+tempo = 0.5
 
 rs, vs, t = sim.Simular(r0, v0, dt, tempo, g=g)
 Simulador.graficos(rs, vs, t, [], a)
