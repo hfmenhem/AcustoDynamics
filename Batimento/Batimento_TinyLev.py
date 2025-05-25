9a262bff79227dfc951acd4d6f67a094ed5b9236
hfmenhem <75636969+hfmenhem@users.noreply.github.com>
2025-05-25 12:56:11 -0300

    Teste de pontos de equilíbrio

--- Batimento/Batimento_TinyLev.py
+++ Batimento/Batimento_TinyLev.py
@@ -29,13 +29,57 @@ g=[0,0,-9.81e3]
 
 #Pontos de equilíbrio 1 partícula
 
-Npar = 1
-r0 = np.array([[[0.0,0,0]]])
-v0 = np.array([[[0.0,0,0]]])
-
 dt = 1e-6
 tcar = m[0,0]/(dicMeio['dinvis']*1e3)#tempo característico devido à força de arrasto
 
+#Partícula no nodo central
+r0 = np.array([[[0.0,0,0]]])
+v0 = np.array([[[0.0,0,0]]])
+
 rs, vs, t = sim.Simular(r0, v0, dt, tcar, g=g)
-Simulador.graficos(rs, vs, t, [], a)
-sim.saveSimulacao(rs, vs, t, 'data')
+
+while not np.all(np.isclose(vs[0,-1, :],0)): #Simula até que a velocidade seja nula
+    rs, vs, t = sim.Simular(rs[:,-1,:], vs[:,-1,:], dt, tcar, g=g)
+    
+req0 = rs[0,-1,:]
+
+#partícula no nodo logo acima do central
+
+r0 = np.array([[[0.0,0,Lamb/2]]])
+v0 = np.array([[[0.0,0,0]]])
+
+rs, vs, t = sim.Simular(r0, v0, dt, tcar, g=g)
+
+while not np.all(np.isclose(vs[0,-1, :],0)): #Simula até que a velocidade seja nula
+    rs, vs, t = sim.Simular(rs[:,-1,:], vs[:,-1,:], dt, tcar, g=g)
+    
+req1 = rs[0,-1,:]
+
+#2 partículas
+r0 = np.array([[[0.0,0,0]],[[0.0,0,Lamb/2]]])
+v0 = np.array([[[0.0,0,0]],[[0.0,0,0]]])
+
+rs, vs, t = sim.Simular(r0, v0, dt, tcar, g=g)
+
+while not np.all(np.isclose(vs[:,-1, :],0)): #Simula até que a velocidade seja nula
+    rs, vs, t = sim.Simular(rs[:,-1,:], vs[:,-1,:], dt, tcar, g=g)
+    
+req01 = rs[:,-1,:]
+
+
+
+print('nodos de pressão, se fossem ondas planas')
+print(f'r0 = {0:.2f} mm')
+print(f'r1 = {Lamb/2:.2f} mm')
+
+print('pontos de equilíbrio, partículas isoladas')
+print(f'r0 = {req0[2]:.2f} mm')
+print(f'r1 = {req1[2]:.2f} mm')
+
+print('pontos de equilíbrio, partículas simultâneas')
+print(f'r0 = {req01[0,2]:.2f} mm')
+print(f'r1 = {req01[1,2]:.2f} mm')
+
+
+#Simulador.graficos(rs, vs, t, [], a)
+#sim.saveSimulacao(rs, vs, t, 'data')
