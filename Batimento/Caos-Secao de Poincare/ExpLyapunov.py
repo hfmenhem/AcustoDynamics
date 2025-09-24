558e8f9df55ea9239bb32b573e6c4b5f76bbbc22
hfmenhem <75636969+hfmenhem@users.noreply.github.com>
2025-09-24 14:31:37 -0300

    Exp. Lyapunov

---
+++ Batimento/Caos-Secao de Poincare/ExpLyapunov.py
@@ -0,0 +1,285 @@
+import sys, os
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+parent_dir = os.path.abspath(os.path.join(parent_dir, ".."))
+sys.path.append(parent_dir) #Para poder importar o main, que está em uma pasta anterior
+
+from main import Simulador
+import numpy as np
+import pickle
+import concurrent.futures
+import time
+#from scipy.integrate import odeint
+from scipy.integrate import solve_ivp
+start_time = time.time()
+
+from matplotlib import pyplot as plt
+
+def Simular(nome,r0, w0):
+    SF = 0
+    kt = 1
+    v0 = [0,0]
+    cond0 = [*r0, 0,0]
+    X1 = [0]
+    while (SF==0):    
+        sol = solve_ivp(SimAc,[(kt-1)*TRen, kt*TRen] , cond0, args=(g,), rtol=rtol, atol=atol, dense_output=True)
+        cond0 = sol.sol(kt*TRen)
+        solT = solve_ivp(SimAcTangente,[(kt-1)*TRen, kt*TRen] , w0, args=(sol,), rtol=rtol, atol=atol, dense_output=False, t_eval=[kt*TRen, ])
+        w0 = solT.y[:, -1]
+        ak = np.linalg.norm(w0)
+        X1.append((X1[len(X1)-1]*((kt-1)*TRen)+np.log(ak))/(kt*TRen))
+        w0 = w0/ak
+        if (kt*TRen > Tmax )or (X1[len(X1)-1]<Xmin):
+            SF=1
+        else:
+            kt = kt+1
+    X1.pop(0)
+    X1 = np.array(X1)
+    ts = (np.arange(len(X1))+1)*TRen
+    salvar = {'r0': r0, 'X1': X1,  'ts': ts}
+    #salvar = {'r0': r0, 'rs': rs, 'vs': vsf, 't': ts}
+
+    # with open(nome, 'wb') as dbfile:
+    #     pickle.dump(salvar, dbfile)
+    #     dbfile.close()
+        
+    return salvar
+
+def SimAcArrasto(t,y, g, C):
+    r = y[:2]
+    v = np.array(y[2:])
+    
+    Fac = np.array([Fz0.ev(*r) ,Fz1.ev(*r)])
+    
+    Far = -6*np.pi*C*v
+    A =( (Fac+Far)/m) + g
+    
+    y = [*v.flatten(), *A.flatten()]
+    return y
+
+def SimAc(t, y, g):
+    r = y[:2]
+    v = np.array(y[2:])
+    
+    Fac = np.array([Fz0.ev(*r) ,Fz1.ev(*r)])
+    
+    A =(Fac/m) + g
+    
+    y = [*v.flatten(), *A.flatten()]
+    return y
+
+def SimAcTangente(t, w, solr):
+    #solr é a solução interpolada da trajetória no espaço de fase
+    dr = w[:2]
+    dv = np.array(w[2:])
+    
+    #Fac = np.array([Fz0.ev(*r) ,Fz1.ev(*r)])
+    r = solr.sol(t)[:2]
+
+    DAac = np.array([[gradFz0[0](*r)[0,0]/m[0], gradFz0[1](*r)[0,0]/m[0]],
+            [gradFz1[0](*r)[0,0]/m[1], gradFz1[1](*r)[0,0]/m[1]]])
+    
+    ddv = np.matmul(DAac, dr)
+ 
+    
+    w = [*dv.flatten(), *ddv.flatten()]
+    return w
+
+f=40e3 #Hz
+dicMeio = Simulador.ar(1)
+
+
+Lamb=dicMeio["c"]/f
+a = np.array([1,1]) # [mm]
+rhoPol = (900*(10**-6)) #[g/mm^3] #Material: PP
+cPol = 2740*(10**3) #[mm/s] 
+m = (a**3*(4*np.pi/3))*rhoPol # [g], densidade do ar vezes seu volume
+
+g=-9.81e3
+
+tsim = 400 #Para espaço de fase, usar valor mais alto
+
+#tsim = 10 #Para expoente de Lyapunov, usar valor mais baixo
+dt = 5e-1
+
+rtol = 1.49012e-11
+atol = 1.49012e-11
+
+if rtol is None:
+    rtol = 1.49012e-8 #Valor padrão usado pela biblioteca
+if atol is None:
+    atol = 1.49012e-8 #Valor padrão usado pela biblioteca
+    
+TRen = 0.1
+Tmax = 10
+Xmin = 0.01
+
+numerosim ='teste-lyapunov'
+
+forca = 'estacionaria'
+
+    
+with open(f'{forca}-força', 'rb') as dbfile:
+    dado = pickle.load(dbfile)
+    dbfile.close()
+
+Fz0 = dado[0]
+Fz1 = dado[1]
+
+gradFz0 = [Fz0.partial_derivative(0,1),Fz0.partial_derivative(1,0)]
+gradFz1 = [Fz1.partial_derivative(0,1),Fz1.partial_derivative(1,0)]
+
+if __name__ == '__main__':
+    #print(f'lambda = {Lamb:.2f} mm ')
+
+    z0eq=[0, Lamb/2]
+
+    padrao = True
+    if padrao:
+        ampdzeq0= [-0.95, -0.9] # amplitude em relação ao ponto de equilíbrio da partícula 0
+        ampdzeq1= [-.875, -0.825] # amplitude em relação ao ponto de equilíbrio da partícula 1
+        
+        ampdzeq0= -1.0/2 # amplitude em relação ao ponto de equilíbrio da partícula 0
+        ampdzeq1=-1.0 # amplitude em relação ao ponto de equilíbrio da partícula 1
+        
+        ampdzeq0=0.0
+        ampdzeq1=0.0
+        
+        Npts0 = 5
+        Npts1 = 5
+    
+        if (type(ampdzeq0) is float or type(ampdzeq0) is int) and (type(ampdzeq1) is float or type(ampdzeq1) is int):
+            dzs0 = np.array(ampdzeq0)
+            dzs1 = np.array(ampdzeq1)
+            ampdzeq0=[ampdzeq0,ampdzeq0]
+            ampdzeq1=[ampdzeq1,ampdzeq1]
+            Npts0=1
+            Npts1=1
+        elif type(ampdzeq0) is float or type(ampdzeq0) is int:
+            dzs0 = np.array(ampdzeq0)
+            dzs1 = np.linspace(ampdzeq1[0], ampdzeq1[1], Npts1, endpoint=True)
+            ampdzeq0=[ampdzeq0,ampdzeq0]
+            Npts0=1
+        elif type(ampdzeq1) is float or type(ampdzeq1) is int:
+            dzs0 = np.linspace(ampdzeq0[0], ampdzeq0[1], Npts0, endpoint=True)
+            dzs1 = np.array(ampdzeq1)
+            ampdzeq1=[ampdzeq1,ampdzeq1]
+            Npts1=1
+        else:
+            dzs1 = np.linspace(ampdzeq1[0], ampdzeq1[1], Npts1, endpoint=True)
+            dzs0 = np.linspace(ampdzeq0[0], ampdzeq0[1], Npts0, endpoint=True)
+        
+        teste=np.meshgrid(dzs0, dzs1)
+        dzs=np.reshape(np.transpose(np.meshgrid(dzs0, dzs1), (1,2,0)), (-1, 2))
+    
+    else:
+        Npts0 = 10
+        #Npts0 = 2
+        Npts1 = 4
+        z1pts = np.array([-0.9, -0.8, -0.7, -0.6])
+        z0inicio = np.linspace(-1, -1+(4*0.07), 4, endpoint=False)
+        z0pts = np.linspace(0, 0.15, Npts0, endpoint=False)
+        z0pts = np.expand_dims(z0inicio, 0)+np.expand_dims(z0pts, 1)
+        z1pts=np.broadcast_to(z1pts, np.shape(z0pts))
+        dzs=np.reshape(np.transpose((z0pts,z1pts), (1,2,0)), (-1, 2))
+        ampdzeq0=[0,0]
+        ampdzeq1=[0,0]
+    
+    #=====================Achar ponto de quilíbrio em 2 partículas=====================
+    
+    C = (dicMeio['dinvis']*1e3)
+    
+    tcar = m[0]/C#tempo característico devido à força de arrasto
+    tscar = np.arange(0, tcar, dt)
+    
+    r0 = np.array(z0eq)
+    v0 = np.zeros(len(z0eq))
+    
+    sol = solve_ivp(SimAcArrasto, [0, tcar], np.array([*r0, *v0]), args=(g,C),  rtol=rtol, atol=atol)
+    #sol = odeint(SimAcArrasto, [*r0, *v0], tscar, args=(g,C))
+    #sol = np.transpose(sol)
+    teste=sol.y
+    rs = sol.y[:2,:]
+    vs = sol.y[2:,:]
+    
+    while not np.all(np.isclose(vs[:,-1],0)): #Simula até que a velocidade seja nula
+        sol = solve_ivp(SimAcArrasto, (0, tcar), [*rs[:,-1], *vs[:,-1]], args=(g,C), rtol=rtol, atol=atol)
+        #sol = np.transpose(sol)
+        rs = sol.y[:2,:]
+        vs = sol.y[2:,:]
+        print(vs[:,-1])
+        
+    req = rs[:,-1] #pontos de equilíbrio
+
+    z0s = np.expand_dims(req, 0)+dzs #Posições iniciais de simulação
+    
+    
+    #=====================Salvamento=====================
+    
+    # if os.path.exists(f'{numerosim}'):
+    #     versao = 2
+    #     while(os.path.exists(f'{numerosim}-v{versao}')):
+    #         versao+=1
+    #     diretorio = f'{numerosim}-v{versao}'
+    # else:
+    #     diretorio = f'{numerosim}'
+    # os.mkdir(diretorio)
+    
+    # arraysalvaValores=[
+    #               #['deslocamento em z', f'{dz:.1f} mm'],
+    #               ['pontos de equilíbrio, partículas simultâneas', ''],
+    #               ['z0', f'{req[0]:.4e} mm'],
+    #               ['z1', f'{req[1]:.4e} mm'],
+    #               ['amplitude de amostragem', ''],
+    #               ['Delta zmin0', f'{ampdzeq0[0]:.4e} mm'],
+    #               ['Delta zmax0', f'{ampdzeq0[1]:.4e} mm'],
+    #               ['Delta zmin1', f'{ampdzeq1[0]:.4e} mm'],
+    #               ['Delta zmax1', f'{ampdzeq1[1]:.4e} mm'],
+    #               ['número de pontos na partícula 0', f'{Npts0}'],
+    #               ['número de pontos na partícula 1', f'{Npts1}'],
+    #               ['tempo de simulação', f'{tsim:.4e} s'],
+    #               ['discretização de salvamento', f'{dt:.4e} s'],
+    #               ['rtol', f'{rtol:.4e} s'],
+    #               ['atol', f'{atol:.4e} s']
+    #               ]
+    
+    
+    # np.savetxt(f'{diretorio}\\geral-trajetorias.txt', arraysalvaValores, fmt='%s')
+    
+    # with open(f'{diretorio}\\pontos', 'wb') as dbfile:
+    #     if padrao:
+    #         pickle.dump([dzs0, dzs1], dbfile)
+    #     else:
+    #         pickle.dump(dzs, dbfile)
+    #     dbfile.close()
+        
+    # with open(f'{diretorio}\\resolucao', 'wb') as dbfile:
+    #     pickle.dump({'rtol': rtol, 'atol': atol, 'dt': dt, 'tsim': tsim}, dbfile)
+    #     dbfile.close()
+    
+    #=====================Religamento=====================
+    
+    #nomes = [f'{diretorio}\\dado-{i}' for i in range(len(z0s))]
+    #yevents = np.full(len(nomes), req[0])
+    
+    resultado = Simular('', z0s[0], [1/np.sqrt(2),1/np.sqrt(2),0,0])#[1/2,1/2,1/2,1/2]
+    
+    # with concurrent.futures.ProcessPoolExecutor() as executor:
+    #     result = executor.map(Simular, nomes, z0s, yevents)
+    
+    # for r in result:
+    #     print(r)
+        
+    print(f'O código demorou {(time.time() - start_time)/60:.1f} min')
+    
+    fig = plt.figure(dpi=300)
+    plt.plot(resultado['ts'], resultado['X1'])
+    plt.xlabel('t[s]')
+    plt.ylabel('$X_1$ [Hz]')
+
+
+
+
+
+
+
+
--- Batimento/Caos-Secao de Poincare/analise-poincare-individual.py
+++ Batimento/Caos-Secao de Poincare/analise-poincare-individual.py
@@ -17,16 +17,115 @@ import concurrent.futures
 
 if __name__ == '__main__':
     
-    pasta='PoincareGrid-con_dev'
+    
+    #===Mapeamento geral
+    # pasta='PoincareGrid4'
+    # n0 = 20
+    # n1 = 20
+    # lista = [(2,0), (2,1),(3,2),#caos?
+    #          (2,2), (7,5), (7,6),#parecido com alto batimento
+    #          (7,4),(17,0), #dentro
+    #           (17,1), (17,2),#alto batimento
+    #           (16,3),(7,7)#+ próximo do equilibrio?
+             
+    #          ]
+    
+    # listacor = [18,17,16,
+    #             13,14,15,
+    #             2,3,
+    #             8,9,
+    #             4,5
+    #             ]
+    
+    # zorderlist=[12,11,10, 
+    #             10.5,7.8,7.5,
+    #             9,8,
+    #             7,6,
+    #             5,4
+    #     ] 
+    # manter = [1-0, 1]
+    
+    # #===Mapeamento geral - próxima da possível separatriz
+    # pasta='PoincareGrid4'
+    # n0 = 20
+    # n1 = 20
+    # lista = [(2,0), (2,1),(3,2),#caos?
+    #          (3,0),
+    #          (4,0)
+    #          ]            
+             
+    
+    # listacor = [18,17,16,
+    #             8,
+    #             15
+                
+    #             ]
+    
+    # zorderlist=[12,11,10, 10,10
+    #     ]
+    # manter = [1-.05, 1]
+    
+    #===Região próxima da possível separatriz
+    # pasta='PoincareGrid-con_dev'
+    # n0 = 5
+    # n1 = 5
+    # lista = [ (4,0),(0,4),(4,2), #circulos
+    #            (1,4), #direita
+    #          (4,1),(3,0),(2,4),(3,3),(4,3),(1,2),(3,1),(2,0),#cima-baixo
+    #          (3,2),#esquerda
+             
+    #          #(0,0),(0,3),
+             
+    #          ]
+  
+
+    # listacor = [9,10, 8,
+    #             18,
+    #             15,14,13,12,4,5,6,7,
+    #             0,
+         
+    #             #0,1,
+    #             ]
+    
+    # zorderlist= np.full(len(lista), 1)
+    # manter = [1-.05, 1]
+    
+    #===Região próxima da possível separatriz, V2
+    pasta='PoincareGrid-con_dev2'
+    n0 = 5
+    n1 = 5
+    lista = [ (4,0),(0,4),(4,2), #circulos
+               (1,4), #direita
+             (4,1),(3,0),(2,4),(3,3),(4,3),(1,2),(3,1),(2,0),#cima-baixo
+             (3,2),#esquerda
+             
+             #(0,0),(0,3),
+             
+             ]
+  
+
+    listacor = [9,10, 8,
+                18,
+                15,14,13,12,4,5,6,7,
+                0,
+         
+                #0,1,
+                ]
+    
+    zorderlist= np.full(len(lista), 1)
+    #manter = [1-.0125, 1]
+    shift=8*0.1
+    comprimento = 0.2
+    manter = [shift, shift+comprimento]
+    print(manter)
+    #manter = [0.75, 1]
+    
     nomes =[]
     for x in os.listdir(pasta):
         if 'dado' in x:
             nomes.append(f'{pasta}\\{x}')
     
    
-    
-    n0 = 5
-    n1 = 5
     
     r0s= []
     
@@ -52,87 +151,40 @@ if __name__ == '__main__':
     inds = np.take_along_axis(inds, arg2, axis=1)
 
     
-    cmappoincare = mpl.colormaps['viridis']
-    
-    # lista = [(2,0), (2,1),(3,2),#caos?
-    #          (2,2), (7,5), (7,6),#parecido com alto batimento
-    #          (7,4),(17,0), #dentro
-    #           (17,1), (17,2),#alto batimento
-    #           (16,3),(7,7)#+ próximo do equilibrio?
-             
-    #          ]
-    
-    # listacor = [18,17,16,
-    #             13,14,15,
-    #             2,3,
-    #             8,9,
-    #             4,5
-    #             ]
-    
-    # zorderlist=[12,11,10, 
-    #             10.5,7.8,7.5,
-    #             9,8,
-    #             7,6,
-    #             5,4
-    #     ]
-    
-    #lista = [(2,0), (2,1),(3,2)]#caos?            
-    #          
-    
-    # listacor = [18,17,16,
-    #             ]
-    
-    # zorderlist=[12,11,10, 
-    #     ]
-    
-    lista = np.transpose(np.reshape(np.meshgrid(np.arange(5), np.arange(5)), (2,-1)))
-    lista = [ (4,0),(0,4),
-             (4,4),  (1,4),
-             (4,1),(3,0),
-             (0,0),(0,3),
-             
-             ]
-
-    listacor = [9,10,
-                19,18,
-                15,14,
-                0,1,
-                ]
-
-                
-    
-    zorderlist= np.full(len(lista), 1)
-    
-
-    
     cmap = mpl.colormaps['tab20b']
     colors= cmap(np.linspace(0,1, 20))
     
     cmap2 = mpl.colormaps['viridis']
     colors2= cmap(np.linspace(0,1, 25))
     
-    fig = plt.figure(dpi=300, figsize=(14,14))
-    ax = fig.add_subplot(projection='3d')
-    #ax.view_init(elev=90, azim=-90)
-    ax.view_init(elev=15, azim=-120)
+    # fig = plt.figure(dpi=300, figsize=(14,14))
+    # ax = fig.add_subplot(projection='3d')
+    # #ax.view_init(elev=90, azim=-90)
+    # ax.view_init(elev=15, azim=-120)
 
-    ax.set_xlabel(r'$z_A$ [mm]')
-    ax.set_ylabel(r'$z_B$ [mm]')
-    ax.set_zlabel(r'$v_B$ [mm/s]')
-    ax.set_title('Espaço de fase seccionado em $v_a=0$')
+    # ax.set_xlabel(r'$z_A$ [mm]')
+    # ax.set_ylabel(r'$z_B$ [mm]')
+    # ax.set_zlabel(r'$v_B$ [mm/s]')
+    # ax.set_title('Espaço de fase seccionado em $v_a=0$')
     # ax.set_ylim(2.5, 5.2)
     # ax.set_xlim(-1.2, 1.2)
     # ax.set_zlim(-400, 400)
     
-    fig2 = plt.figure(dpi=300, figsize=(10,10))
+    fig2 = plt.figure(dpi=300, figsize=(30,30))
     ax2 = fig2.add_subplot()
     ax2.set_xlabel(r'$z_A$ [mm]')
     ax2.set_ylabel(r'$z_B$ [mm]')
     ax2.set_title('Espaço de fase seccionado em $v_a=0$ projetado em $z_a \\times z_b$')
     ax2.set_aspect(1)
     
-    manter = .05
-    #anter = 1
+    # fig3 = plt.figure(dpi=300, figsize=(20,20))
+    # ax3 = fig3.add_subplot()
+    # ax3.set_xlabel(r'$z_A$ [mm]')
+    # ax3.set_ylabel(r'$v_B$ [mm/s]')
+    # ax3.set_title('Espaço de fase seccionado em $v_a=0$ projetado em $z_a \\times v_b$')
+    #ax2.set_aspect(1)
+    
+    
     for i, coord in enumerate(lista):
         ind = inds[tuple(coord)]
         nome = nomes[ind]
@@ -144,18 +196,27 @@ if __name__ == '__main__':
         rpic = dado['rpic']
         vpic = dado['vpic']
 
-        descarten = int(len(rpic[0,:])*manter)
+        manterLen = np.round(len(rpic[0,:])*np.array(manter)).astype(int)
         
-        ax.plot( rpic[0,-1*descarten:], rpic[1,-1*descarten:], vpic[1,-1*descarten:], '.', markersize=1, color = colors[listacor[i]], zorder=zorderlist[i] )
-        ax.plot(r0[0], r0[1],0, marker='+', color = colors[listacor[i]],zorder=zorderlist[i])
+        # ax.plot( rpic[0,manterLen[0]:manterLen[1]], rpic[1,manterLen[0]:manterLen[1]], vpic[1,manterLen[0]:manterLen[1]], '.', markersize=1, color = colors[listacor[i]], zorder=zorderlist[i] )
+        # ax.plot(r0[0], r0[1],0, marker='+', color = colors[listacor[i]],zorder=zorderlist[i])
         #ax.plot(rpic[1,0], vpic[1,0], rpic[0,0], marker='x', color = colors[i])
-        ax2.plot( rpic[0,-1*descarten:], rpic[1,-1*descarten:], '.', markersize=1, color = colors[listacor[i]], zorder=zorderlist[i] )
+        ax2.plot( rpic[0,manterLen[0]:manterLen[1]], rpic[1,manterLen[0]:manterLen[1]], '.', markersize=1, color = colors[listacor[i]], zorder=zorderlist[i] )
         ax2.plot(r0[0], r0[1], marker='+', color = colors[listacor[i]],zorder=zorderlist[i])
 
-    
+        # ax3.plot( rpic[0,manterLen[0]:manterLen[1]], vpic[1,manterLen[0]:manterLen[1]], '.', markersize=1, color = colors[listacor[i]], zorder=zorderlist[i] )
+        # ax3.plot(r0[0], 0, marker='+', color = colors[listacor[i]],zorder=zorderlist[i])
+
+
     # ax.set_xlim(-1.1,-.9)
     # ax.set_ylim(3.2,3.4)
     # ax.set_zlim(-30,30)
+    
+    ax2.set_xlim(-1.1,-.9)
+    ax2.set_ylim(3.2,3.4)
+    
+    # ax3.set_xlim(-1.1,-.9)
+    # ax3.set_ylim(-30,30)
     
     plt.show()
         
@@ -166,4 +227,3 @@ if __name__ == '__main__':
 
     
    
-
